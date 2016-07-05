123fd93637eecd2b705098b8b932b73609e9620a
pavel1yakimovich <pavel1yakimovich@gmail.com>
2016-07-05 19:14:58 +0300

    made algorithm for copying and added nunit tests

---
+++ EPAM.Summer.Yakimovich.03/Task03.NUnitTests/CopyTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using static Task03.Copy;
+
+namespace Task03.NUnitTests
+{
+    public class CopyTests
+    {
+        [TestCase(15, 3, 3, 5, Result = 31)]
+        [TestCase(15, 5, 5, 6, Result = 47)]
+        [TestCase(-15, 5, 0, 1, Result = -15)]
+        [TestCase(15, -5, 2, 5, Result = 47)]
+        [TestCase(-15, -5, 7, 11, Result = -15)]
+        public int CopyMethod_OK(int first, int second, int startPos, int endPos)
+        {
+            return CopyMethod(first, second, startPos, endPos);
+        }
+
+        [TestCase(15, 5, 5, 4, ExpectedException = typeof(ArgumentException))]
+        public int CopyMethod_ArgumentException(int first, int second, int startPos, int endPos)
+        {
+            return CopyMethod(first, second, startPos, endPos);
+        }
+
+        [TestCase(15, 5, -9, 4, ExpectedException = typeof(ArgumentOutOfRangeException))]
+        [TestCase(15, 5, 2, 31, ExpectedException = typeof(ArgumentOutOfRangeException))]
+        [TestCase(15, 5, 31, 31, ExpectedException = typeof(ArgumentOutOfRangeException))]
+        [TestCase(15, 5, -2, -1, ExpectedException = typeof(ArgumentOutOfRangeException))]
+        public int CopyMethod_ArgumentOutOfRangeException(int first, int second, int startPos, int endPos)
+        {
+            return CopyMethod(first, second, startPos, endPos);
+        }
+    }
+}
---
+++ EPAM.Summer.Yakimovich.03/Task03/Copy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    public static class Copy
+    {
+        private static int MaxBit = 30;
+        /// <summary>
+        /// method inserts first number in second. second number will be from startPos bit to endPos bit.
+        /// </summary>
+        /// <param name="first">number that we insert</param>
+        /// <param name="second">number, we insert in</param>
+        /// <param name="startPos">start number of bit</param>
+        /// <param name="endPos">end number of bit</param>
+        /// <returns>inserted number</returns>
+        public static int CopyMethod(int first, int second, int startPos, int endPos)
+        {
+            if (startPos < 0 || endPos > MaxBit)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (startPos > endPos)
+            {
+                throw new ArgumentException();
+            }
+
+            int k = (int) (Math.Pow(2, endPos - startPos + 1) - 1);
+            int n = int.MinValue + k;
+            second = second & n;
+            second = second << startPos;
+
+            return first | second;
+        }
+    }
+}
